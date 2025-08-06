06d9bb17ab19b1e691198c30dfb44deb1d8d845c
Brendan <brendanlawless@hotmail.com>
2025-08-06 17:24:14 +0100

    chore: added esbuild && typescript

---
+++ build.js
@@ -0,0 +1,32 @@
+// build.js
+const esbuild = require('esbuild');
+const { livereloadPlugin } = require('@jgoz/esbuild-plugin-livereload');
+
+async function start() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    outfile: 'public/js/bundle.js',
+    format: 'esm',
+    target: 'es6',
+    sourcemap: true,
+    logLevel: 'silent', 
+    plugins: [livereloadPlugin()],
+  });
+
+  // Start watch mode
+  await ctx.watch();
+  console.log('Watching for changes...');
+
+  // Start the development server
+  await ctx.serve({
+    servedir: 'public',
+    port: 8080,
+  });
+  console.log('Server running at http://localhost:8080');
+}
+
+start().catch((error) => {
+  console.error('Build failed:', error);
+  process.exit(1);
+});
